eac9264c7099434d2cbff81e3a45696719a176dd
ajinkya.khedekar52 <ajinkya.khedekar52@gmail.com>
2019-01-04 10:43:19 +0530

    new files

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -1,26 +1,16 @@
 <template>
   <div class="container">
-    <div class="row">
-      <div class="col-12">
+    <div class="row mt-5">
+      <div class="col-md-12">
         <div class="card">
           <div class="card-header">
-            <h3 class="card-title">Responsive Hover Table</h3>
+            <h3 class="card-title">Users Table</h3>
 
             <div class="card-tools">
-              <div class="input-group input-group-sm" style="width: 150px;">
-                <input
-                  type="text"
-                  name="table_search"
-                  class="form-control float-right"
-                  placeholder="Search"
-                >
-
-                <div class="input-group-append">
-                  <button type="submit" class="btn btn-default">
-                    <i class="fa fa-search"></i>
-                  </button>
-                </div>
-              </div>
+              <button class="btn btn-success" data-toggle="modal" data-target="#addNew">
+                Add New
+                <i class="fas fa-user-plus fa-fw"></i>
+              </button>
             </div>
           </div>
           <!-- /.card-header -->
@@ -29,10 +19,10 @@
               <tbody>
                 <tr>
                   <th>ID</th>
-                  <th>User</th>
-                  <th>Date</th>
-                  <th>Status</th>
-                  <th>Reason</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Type</th>
+                  <th>Modify</th>
                 </tr>
                 <tr>
                   <td>183</td>
@@ -41,34 +31,15 @@
                   <td>
                     <span class="tag tag-success">Approved</span>
                   </td>
-                  <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                </tr>
-                <tr>
-                  <td>219</td>
-                  <td>Alexander Pierce</td>
-                  <td>11-7-2014</td>
                   <td>
-                    <span class="tag tag-warning">Pending</span>
+                    <a href="#">
+                      <i class="fas fa-edit blue"></i>
+                    </a>
+                    |
+                    <a href="#">
+                      <i class="fas fa-trash red"></i>
+                    </a>
                   </td>
-                  <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                </tr>
-                <tr>
-                  <td>657</td>
-                  <td>Bob Doe</td>
-                  <td>11-7-2014</td>
-                  <td>
-                    <span class="tag tag-primary">Approved</span>
-                  </td>
-                  <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                </tr>
-                <tr>
-                  <td>175</td>
-                  <td>Mike Doe</td>
-                  <td>11-7-2014</td>
-                  <td>
-                    <span class="tag tag-danger">Denied</span>
-                  </td>
-                  <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
                 </tr>
               </tbody>
             </table>
@@ -78,11 +49,111 @@
         <!-- /.card -->
       </div>
     </div>
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      id="addNew"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="addNewLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="addNewLabel">Add New</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <form @submit.prevent="createUser">
+          <div class="modal-body">
+            <div class="form-group">
+              <input
+                v-model="form.name"
+                type="text"
+                name="name"
+                class="form-control"
+                :class="{ 'is-invalid': form.errors.has('name') }"
+                placeholder="Name"
+              >
+              <has-error :form="form" field="name"></has-error>
+            </div>
+
+            <div class="form-group">
+              <input
+                v-model="form.email"
+                type="email"
+                name="email"
+                class="form-control"
+                :class="{ 'is-invalid': form.errors.has('email') }"
+                placeholder="Email"
+              >
+              <has-error :form="form" field="email"></has-error>
+            </div>
+
+            <div class="form-group">
+              <textarea
+                v-model="form.bio"
+                name="bio"
+                class="form-control"
+                :class="{ 'is-invalid': form.errors.has('bio') }"
+                placeholder="Short Bio"
+              ></textarea>
+              <has-error :form="form" field="bio"></has-error>
+            </div>
+
+            <div class="form-group">
+              <select
+                name="type"
+                v-model="form.type"
+                id="type"
+                class="form-control"
+                :class="{ 'is-invalid': form.errors.has('type')}"
+              >
+                <option value>Select User Role</option>
+                <option value="admin">Admin</option>
+                <option value="user">Standard User</option>
+                <option value="author">Author</option>
+              </select>
+              <has-error :form="form" field="type"></has-error>
+            </div>
+
+            <div class="form-group">
+              <input v-model="form.password" type="password" name="password" id="password" class="form-control" :class="{ 'is-invalid': form.errors.has('password')}" placeholder="Password">
+              <has-error :form="form" field="password"></has-error>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Create</button>
+          </div>
+          </form>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      form: new Form({
+        name: "",
+        email: "",
+        password: "",
+        type: "",
+        bio: "",
+        photo: ""
+      })
+    };
+  },
+  methods: {
+      createUser(){
+          this.form.post('api/user');
+      }
+  },
   mounted() {
     console.log("Component mounted.");
   }
